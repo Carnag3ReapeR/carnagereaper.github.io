f65367c287bb1e67416136f66d0ded19a576c2d9
Mikhaelin <ramsaroopm@gmail.com>
2023-05-06 00:32:58 +0200

    Initial Test Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from "./Components/Navbar"
+import Test from "./Components/Test"
+import About from "./Components/About"
+import Skills from "./Components/Skills"
+import Gaming from "./Components/Gaming"
+import Videos from "./Components/Videos"
+import Contact from "./Components/Contact"
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold text-red-500 underline text-center">Hello world!</h1> 
+    <main className='text-blue-400 bg-black body-font'> 
+      <Navbar />
+      <Test />
+      <About />
+      <Skills />
+      <Gaming />
+      <Videos />
+      <Contact />
+    </main>
   );
 }
 
