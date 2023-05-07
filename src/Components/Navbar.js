c79939afe872a635a9c68721ce41c7df431ae173
Mikhaelin <ramsaroopm@gmail.com>
2023-05-07 22:41:37 +0200

    Navbar Updates

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,29 +1,30 @@
 
 import React from "react";
+import prof from "./Images/text.jpg"
 
 export default function Navbar(){
     return(
-        <header className="bg-black md:sticky top-0 z-10">
+        <header className="bg-gray-800 md:sticky top-0 z-0">
             <div className="container mx-auto flex flew-wrap p-5 flex-col md:flex-row items-center">
-                <a className="title-font font-medium text-white mb-4 md:mb-0">
+                <a className="title-font font-semibold text-teal-500 mb-4 md:mb-0">
                     <a href="#About" className="ml-3 text-xl">
-                        About Me
+                        Carnag3ReapeR
                     </a>
                 </a>
-                <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-white	flex flex-wrap items-center text-base justify-center">
-                    <a href="#Contact" className="mr-5 hover:text-white">
+                <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l-2 md:border-teal-800 flex flex-wrap items-center text-base justify-center">
+                    <a href="#Contact" className="mr-5 hover:text-white md:border-r-2 md:border-teal-800 md:pr-4">
                         Contact
                     </a>
-                    <a href="#Gaming" className="mr-5 hover:text-white">
+                    <a href="#Gaming" className="mr-5 hover:text-white md:border-r-2 md:border-teal-800 md:pr-4">
                         Gaming
                     </a>
-                    <a href="#Skills" className="mr-5 hover:text-white">
+                    <a href="#Skills" className="mr-5 hover:text-white md:border-r-2 md:border-teal-800 md:pr-4">
                         Skills
                     </a>
+                    <a href="#Videos" className="mr-5 hover:text-white md:border-r-2 md:border-teal-800 md:pr-4">
+                        Videos
+                    </a>
                 </nav>
-                <a href="#Videos" className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-blue-700 rounded text-base mt-4 md:mt-0">
-                    Videos
-                </a>
             </div>
         </header>
     );
---
+++ src/Components/data.js
@@ -0,0 +1,7 @@
+export const details = [
+    {
+    fullname: "Mikhaelin Ramsaroop",
+    IGN: "Carnag3ReapeR",
+    Email: "Mikhaelin.Ramsaroop@gmail.com"
+    }
+]
