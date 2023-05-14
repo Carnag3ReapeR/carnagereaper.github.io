394c70935068748b52964b1df3592b3bd1f1b9e1
Mikhaelin <ramsaroopm@gmail.com>
2023-05-14 14:11:39 +0200

    Added Fontawesome

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ import Skills from "./Components/Skills"
 import Gaming from "./Components/Gaming"
 import Videos from "./Components/Videos"
 import Contact from "./Components/Contact"
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faInfoCircle,faCode,faPerson} from '@fortawesome/free-solid-svg-icons';
 
 function App() {
+  library.add(faInfoCircle,faCode,faPerson)
   return (
     <main className='text-blue-400 bg-black body-font'> 
       <Navbar />
--- src/Components/About.js
+++ src/Components/About.js
@@ -4,16 +4,20 @@ import { personalDetails } from './data';
 
 export default function About(){
     return(
-        <section id="About" className="text-gray-400 bg-gray-900 body-font">
+        <section id="About" className="text-blue-400 bg-black body-font border-2">
             {
               personalDetails.map((values) =>
                 <div className='container px-5 py-10 mx-auto text-center lg:px-40'>
                     <div className='flex flex-col w-full mb-20'>
-                        <FontAwesomeIcon icon={["fas", "github"]}></FontAwesomeIcon>
                         <h1 className='title-font font-semibold sm:text-4xl text-3xl mb-4 text-teal-400'>
-                            About Me
+                        <FontAwesomeIcon icon="info-circle" /> About Me
                         </h1>
-
+                        <div className='grid grid-cols-2 gap-3'>
+                            <div className='flex'>
+                                <img src={profilePic} className='object-contain rounded-full'></img>
+                            </div>
+                            <div>2</div>
+                        </div>
                     </div>
                     
                 </div>
